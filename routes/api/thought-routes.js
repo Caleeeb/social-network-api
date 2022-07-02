17eba57601364a64d7e38143634dcf5aaa0ef8ba
Caleeeb <daycinematography@gmail.com>
2022-07-02 16:47:15 -0400

    thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+
+// the requirements
+const {
+	createThought,
+	getAllThoughts,
+	getThoughtById,
+	updateThought,
+	deleteThought,
+	newReaction,
+	deleteReaction,
+} = require("../../controllers/thoughts-controller");
+
+// ROUTE: /api/thoughts
+router.route("/").get(getAllThoughts);
+
+// ROUTE: /api/thoughts/:id
+router
+	.route("/:id")
+	.get(getThoughtById)
+	.put(updateThought)
+	.delete(deleteThought);
+
+// ROUTE: /api/thoughts/:userId
+router.route("/:userId").post(createThought).delete(deleteThought);
+
+// ROUTE: /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(newReaction);
+
+// ROUTE: /api/thoughts/:thoughtId/reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+module.exports = router;
